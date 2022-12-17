82b23b13d66379c268baa45fc724d34c1a11fd95
renanlisboa <renlp89.developer@gmail.com>
2022-12-17 00:14:10 -0300

    test: dispatch event in event dispatcher

--- tests/event-dispatcher.spec.ts
+++ tests/event-dispatcher.spec.ts
@@ -1,5 +1,6 @@
-import { EventDispatcher } from '../src/event-dispatcher.entity'
-import { MailerEventHandler } from '../src/mailer-event-handler'
+import { EventDispatcher } from '../src/infra/event-dispatcher'
+import { MailerEventHandler } from '../src/application/event-handlers/mailer.event-handler'
+import { OrderPlacedEvent } from '../src/domain/events'
 
 describe('EventDispatcher', () => {
   it('should register an event', async () => {
@@ -11,5 +12,23 @@ describe('EventDispatcher', () => {
 
     expect(eventDispatcher.register).toHaveBeenCalledWith(mailerEventHandler)
     expect(eventDispatcher.eventHandlers).toContain(mailerEventHandler)
+  })
+
+  it('should dispatch an event', () => {
+    const eventDispatcher = new EventDispatcher()
+    const mailerEventHandler = new MailerEventHandler()
+    eventDispatcher.register(mailerEventHandler)
+    const orderPlacedEvent = new OrderPlacedEvent({
+      orderId: '1a2b3c',
+      customerId: 'a1b2c3',
+      itemId: 'c3b2a1',
+      quantity: 1,
+      total: 100,
+    })
+    jest.spyOn(mailerEventHandler, "handle")
+
+    eventDispatcher.dispatch(orderPlacedEvent)
+    
+    expect(mailerEventHandler.handle).toHaveBeenCalledWith(orderPlacedEvent)
   })
 })
